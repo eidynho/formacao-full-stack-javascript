dee610f1330bc158294067e7f7929a169ecdc338
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-03-14 18:40:41 -0300

    nodeJS básico

---
+++ 18 - NodeJS/aula5/server/index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer(function(request, response) {
+
+    if (request.url === '/') {
+        fs.readFile('../client/index.html', function (error, content) {
+            response.end(content)
+        })
+    }
+    if (request.url === '/teste') {
+        response.end('ok, funcionou')
+    }
+})
+
+server.listen(8080)
+console.log('Servidor escutando na porta 8080')
