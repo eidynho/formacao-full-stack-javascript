ff57482e03e7496fe5f085133f7bd69c710a4f81
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-03-18 18:50:06 -0300

    refatorando codigo js

--- 17 - Projeto de teste para vaga de emprego/Teste - Juros compostos/js/main.js
+++ 17 - Projeto de teste para vaga de emprego/Teste - Juros compostos/js/main.js
@@ -1,34 +1,63 @@
+const Main = {
 
-const name = document.querySelector('#inputName').value
-const inputMonthly = document.querySelector('#inputMonthly')
-const selectTime = document.querySelector('#select-time') //teste (e ver o .value)
-const interestRate = document.querySelector('#interest-rate')
-const btnSimular = document.querySelector('#btn-simular')
+    init: function() {
+        this.cacheSelectors()
+        this.bindEvents()
+    },
 
+    cacheSelectors: function() {
+        this.$name = document.querySelector('#inputName').value
+        this.$inputMonthly = document.querySelector('#inputMonthly')
+        this.$selectTime = document.querySelector('#select-time')
+        this.$interestRate = document.querySelector('#interest-rate')
+        this.$btnSimular = document.querySelector('#btn-simular')
+    },
 
+    bindEvents: function() {
+        const self = this
 
-btnSimular.onclick = async(e) => {
-    e.preventDefault()
+        this.$btnSimular.onclick = self.Events.simulateButton_click.bind(this)
+    },
 
-    const configs = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-
-            "expr": [
-                `${inputMonthly.value} * (((1 + ${interestRate.value}) ^ ${selectTime.value} - 1) / ${interestRate.value})`
-            ]
-            
-        }),
-    }
-
-
-    await fetch ('http://api.mathjs.org/v4/', configs)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+    Events: {
+        simulateButton_click: async function(e) {
+            e.preventDefault()
         
+            const configs = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+        
+                    "expr": [
+                        `${this.$inputMonthly.value} * (((1 + ${this.$interestRate.value}) ^ ${this.$selectTime.value} - 1) / ${this.$interestRate.value})`
+                    ]
+                    
+                }),
+            }
+        
+            await fetch ('http://api.mathjs.org/v4/', configs)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.log(error))
+        }
+    }
 }
+
+Main.init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
