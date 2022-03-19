4f7ab68e3ac53d7ce72893eb36e4d3b967096648
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-03-19 18:02:07 -0300

    adicionando resultado da simulacao e botao refresh

--- 17 - Projeto de teste para vaga de emprego/Teste - Juros compostos/js/main.js
+++ 17 - Projeto de teste para vaga de emprego/Teste - Juros compostos/js/main.js
@@ -6,23 +6,27 @@ const Main = {
     },
 
     cacheSelectors: function() {
-        this.$name = document.querySelector('#inputName').value
+        this.$title = document.querySelector('.title')
+        this.$form = document.querySelector('#form')
+        this.$name = document.querySelector('#inputName')
         this.$inputMonthly = document.querySelector('#inputMonthly')
         this.$selectTime = document.querySelector('#select-time')
         this.$interestRate = document.querySelector('#interest-rate')
         this.$btnSimular = document.querySelector('#btn-simular')
+        this.$divResult = document.querySelector('.result')
+        this.$btnRefresh = document.querySelector('.btn-refresh')
     },
 
     bindEvents: function() {
         const self = this
 
         this.$btnSimular.onclick = self.Events.simulateButton_click.bind(this)
+        this.$btnRefresh.onclick = self.Events.refreshButton_click
     },
 
     Events: {
         simulateButton_click: async function(e) {
-            e.preventDefault()
-        
+           e.preventDefault()
             const configs = {
                 method: 'POST',
                 headers: {
@@ -31,16 +35,31 @@ const Main = {
                 body: JSON.stringify({
         
                     "expr": [
-                        `${this.$inputMonthly.value} * (((1 + ${this.$interestRate.value}) ^ ${this.$selectTime.value} - 1) / ${this.$interestRate.value})`
+                        `${this.$inputMonthly.value} * (((1 + ${this.$interestRate.value/100}) ^ ${this.$selectTime.value} - 1) / ${this.$interestRate.value/100})`
                     ]
                     
-                }),
+                })
             }
         
             await fetch ('http://api.mathjs.org/v4/', configs)
                 .then(response => response.json())
-                .then(data => console.log(data))
+                .then(data => {
+                    const result = Number(data.result).toFixed(2)
+                    const resultString = result.toString().replace(".", ",")
+
+                    this.$title.innerHTML = 'Resultado'
+                    this.$form.classList.add('none')
+                    this.$divResult.innerHTML += `
+                        Olá ${this.$name.value}, juntando R$ ${this.$inputMonthly.value} todo mês, você terá <strong>R$ ${resultString}</strong> em ${this.$selectTime.value} meses.
+                    `
+                    this.$btnRefresh.classList.remove('none')
+                    
+                })
                 .catch(error => console.log(error))
+        },
+
+        refreshButton_click: function() {
+            location.reload()
         }
     }
 }
